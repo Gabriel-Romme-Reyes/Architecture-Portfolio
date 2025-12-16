0838d86142169eda74664cdc9d9d7e8c4ecedc74
Gabriel <gabrielrommeleisure@gmail.com>
2025-12-16 15:56:13 +0800

    added ci/cd lol

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
+  site: 'https://Gabriel-Romme-Reyes.github.io',
+  base: '/Architecture_Portfolio/',
 });
