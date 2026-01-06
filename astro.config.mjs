e9df1e8f59806525b5d4204fc627ff951f663fb0
Gabriel <gabrielrommeleisure@gmail.com>
2026-01-07 02:43:02 +0800

    changed astro stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   output: 'static',
   site: 'https://Gabriel-Romme-Reyes.github.io',
-  base: '/Architecture-Portfolio/',
+  base: '/portfolio/',
 });
